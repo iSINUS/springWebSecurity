8b2fa02515c7e6fd76ec47c2b1ac64f8261d5ab8
Sergei Ivanov <sinus@Sergeis-MBP.lan>
2016-02-21 10:05:58 +0300

    Initial commit

---
+++ src/main/java/com/sinus/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.sinus;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by sinus on 19.02.16.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
---
+++ src/main/java/com/sinus/SpringWebSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.sinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSecurityApplication.class, args);
+	}
+}
